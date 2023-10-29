1ac0f50d0f0dcf4922e9a6cfd355654d9c0d5dac
stopzero <magic2eye3@gmail.com>
2023-10-29 22:05:40 +0900

    11

---
+++ LV.0/배열의 평균.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = sum(numbers)/len(numbers)
+    
+    return answer
+
+
+
+#     문제 설명
+# 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
---
+++ lv.1/평균구하기.py
@@ -0,0 +1,15 @@
+def solution(arr):
+    return sum(arr)/len(arr)
+
+
+
+
+# 문제 설명
+# 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+
+# 제한사항
+# arr은 길이 1 이상, 100 이하인 배열입니다.
+# arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+
+#배운점: sum(arr)은 배열을 더하는것, len(arr)은 배열의 길이를 제는것
+#즉 arr: [1,2,3]이 들어오면 sum(arr)=6 len(arr)=3 이다.
