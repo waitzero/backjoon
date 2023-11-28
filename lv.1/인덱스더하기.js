10e160f9d617e9082d1d0d814e32cdaaee9e2582
waitzero <magic2eye3@gmail.com>
2023-11-29 05:30:10 +0900

    1129

--- lv.1/인덱스더하기.js
+++ lv.1/인덱스더하기.js
@@ -3,8 +3,19 @@ function solution(numbers) {
     
     for(let i = 0; i < numbers.length; i++) {
         for(let j = i + 1; j < numbers.length; j++) {
-            answer.push(numbers[i] + numbers[j]);
+            const sum = numbers[i] + numbers[j];
+            if(!answer.includes(sum)){
+                answer.push(sum)
+            }
         }
     }
-    return [...new Set(answer.sort())];
+    answer.sort((a,b)=> a-b)
+    
+    return answer;
 }
+
+// 문제 설명
+// 정수 배열 numbers가 주어집니다. 
+// numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 
+// 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아
+//  return 하도록 solution 함수를 완성해주세요.
